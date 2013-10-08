51b9977894f9f617815735da4b28c8f54cb316f9
Henrik Bengtsson <henrikb@braju.com>
2013-10-08 19:45:11 +0000

    version 1.5.2

--- R/isGenericS3.R
+++ R/isGenericS3.R
@@ -1,7 +1,7 @@
 ###########################################################################/**
 # @RdocDefault isGenericS3
 #
-# @title "Checks if a function is a S3/UseMethod generic function"
+# @title "Checks if a function is a S3 generic function"
 #
 # \description{
 #  @get "title".
@@ -17,9 +17,9 @@
 # }
 #
 # \details{
-#   A function is considered to be a generic S3/UseMethod function if its
-#   body, that is the source code, contains the regular pattern 
-#   \code{"UseMethod[(]"}.
+#   A function is considered to be a generic S3/UseMethod function if
+#   its name matches one of the known S3 generic functions, or if it
+#   calls \code{UseMethod()}.
 # }
 #
 # \value{
@@ -28,20 +28,103 @@
 #
 # @author
 #
-# @keyword "programming"
-# @keyword "methods"
-# @keyword "internal"
+# @keyword programming
+# @keyword methods
 #*/###########################################################################
 isGenericS3.default <- function(fcn, envir=parent.frame(), ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  knownInternalGenericS3 <- function(fcn, which=1:4, ...) {
+    knownGenerics <- NULL;
+
+    # Get the name of all known S3 generic functions
+    if (any(which == 1L)) {
+      knownGenerics <- c(knownGenerics, names(.knownS3Generics));
+    }
+
+    if (any(which == 2L)) {
+      knownGenerics <- c(knownGenerics, .S3PrimitiveGenerics);
+    }
+
+    # tools:::.get_internal_S3_generics() if available
+    if (any(which == 3L)) {
+      ns <- getNamespace("tools")
+      if (exists(".get_internal_S3_generics", envir=ns, inherits=FALSE)) {
+        names <- get(".get_internal_S3_generics", envir=ns, inherits=FALSE)();
+        knownGenerics <- c(knownGenerics, names);
+      }
+    }
+
+    # Manually added, cf. ?cbind
+    if (any(which == 4L)) {
+      names <- c("cbind", "rbind");
+      knownGenerics <- c(knownGenerics, names);
+    }
+
+    # Is it one of the known S3 generic functions?
+    knownGenerics <- unique(knownGenerics);
+
+    knownGenerics;
+  } # knownInternalGenericS3()
+
+  isNameInternalGenericS3 <- function(fcn, ...) {
+    is.element(fcn, knownInternalGenericS3());
+  } # isNameInternalGenericS3()
+
+  isPrimitive <- function(fcn, ...) {
+    switch(typeof(fcn), special=TRUE, builtin=TRUE, FALSE)
+  } # isPrimitive()
+
+
   if (is.character(fcn)) {
-    fcn <- get(fcn, mode="function", envir=envir, inherits=TRUE);
+    if (isNameInternalGenericS3(fcn)) return(TRUE);
+
+    # Get the function
+    fcn <- .findFunction(fcn, envir=envir, inherits=TRUE)$fcn;
+
+    # Does it even exist?
+    if (is.null(fcn)) {
+      return(FALSE);
+    }
   }
-  body <- body(fcn);
-  if (is.call(body))
-    body <- deparse(body);
-  body <- as.character(body);
-  return(length(grep("UseMethod[(]", body)) > 0)
+
+  # Check with codetools::findGlobals(), if available,
+  # otherwise scan the body
+  res <- tryCatch({
+    ns <- getNamespace("codetools");
+    findGlobals <- get("findGlobals", mode="function", envir=ns);
+    fcns <- findGlobals(fcn, merge=FALSE)$functions;
+    is.element("UseMethod", fcns);
+  }, error = function(ex) {
+    # Scan the body of the function
+    body <- body(fcn);
+    if (is.call(body))
+      body <- deparse(body);
+    body <- as.character(body);
+    (length(grep("UseMethod[(]", body)) > 0L);
+  });
+  if (isTRUE(res)) return(TRUE);
+
+  # Check primitive functions
+  if (isPrimitive(fcn)) {
+    # Scan the body of the function
+    body <- deparse(fcn);
+    call <- grep(".Primitive[(]", body, value=TRUE);
+    call <- gsub(".Primitive[(]\"", "", call);
+    call <- gsub("\"[)].*", "", call);
+    if (is.element(call, knownInternalGenericS3(2L))) return(TRUE);
+  }
+
+  # Finally, compare to all known internal generics
+  for (name in knownInternalGenericS3()) {
+    generic <- get(name, mode="function", inherits=TRUE);
+    if (identical(fcn, generic)) return(TRUE);
+  }
+
+  FALSE;
 }
+S3class(isGenericS3.default) <- "default";
 export(isGenericS3.default) <- FALSE;
 
 setGenericS3("isGenericS3");
@@ -66,7 +149,7 @@ setGenericS3("isGenericS3");
 #
 # \details{
 #   A function is considered to be a generic S4 function if its
-#   body, that is the source code, contains the regular pattern 
+#   body, that is the source code, contains the regular pattern
 #   \code{"standardGeneric"}.
 # }
 #
@@ -90,6 +173,7 @@ isGenericS4.default <- function(fcn, envir=parent.frame(), ...) {
   body <- as.character(body);
   return(length(grep("standardGeneric", body)) > 0)
 }
+S3class(isGenericS4.default) <- "default";
 export(isGenericS4.default) <- FALSE;
 
 setGenericS3("isGenericS4");
@@ -98,6 +182,10 @@ setGenericS3("isGenericS4");
 
 ############################################################################
 # HISTORY:
+# 2013-10-05
+# o ROBUSTNESS: Now isGenericS3() also compares to known generic functions
+#   in the 'base' package.  It also does a better job on checking whether
+#   the function calls UseMethod() or not.
 # 2010-09-18
 # o BUG FIX: isGenericS3() and isGenericS4() did not support specifying
 #   the function by name as a character string, despite it was documented
